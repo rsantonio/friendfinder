74beb36a14f50ae9fe2ca10e9fefd921028e551e
Ruth San Antonio <RuthS.Antonio@gmail.com>
2017-11-04 07:26:00 -0700

    update updata

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,15 +1,15 @@
 var potentialFriends = [ 
 	{
 		"name" : "Tim",
-		"photo" : "",
+		"photo" : "https://www.google.com/search?q=huskies&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi17b21i6XXAhUD2WMKHWehB2gQ_AUICigB&biw=767&bih=741&dpr=2.5#imgrc=AGZiZxoiL8lW6M:",
 		"scores" : [3,3,3,3,3,3,3,3,3,5]
 	}, {
 		"name" : "Sasha",
-		"photo" : "",
+		"photo" : "https://www.google.com/search?q=huskies&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi17b21i6XXAhUD2WMKHWehB2gQ_AUICigB&biw=767&bih=741&dpr=2.5#imgrc=wZ7ISCj337P_kM:",
 		"scores" : [2,4,5,1,2,1,1,1,1,5]
 	}, {
 		"name" : "Viserys",
-		"photo" : "",
+		"photo" : "https://www.google.com/search?q=huskies&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi17b21i6XXAhUD2WMKHWehB2gQ_AUICigB&biw=767&bih=741&dpr=2.5#imgrc=RMyJ5pd1-GujNM:",
 		"scores" : [3,4,5,1,3,1,1,2,1,5]
 	}
 
